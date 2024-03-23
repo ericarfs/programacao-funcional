c5bc4e9d6cf6fff05385c91e4d58f8c89c3e3a6f
ericarfs <ericaribeiro@usp.br>
2024-03-23 00:27:27 -0300

    update

---
+++ Atividade 01/Casos_de_Teste/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 22/03/2024 18:55:04 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Atividade 01
---
+++ Atividade 01/main.py
@@ -0,0 +1,62 @@
+def cabeca(a): 
+    return a[0]
+
+def cauda(a):
+    return a[1:]
+
+def num_primo(numero, fator): 
+    if fator == numero:
+        return True 
+    
+    if numero % fator == 0:
+        return False
+        
+    return num_primo(numero, fator + 1)
+
+
+def listar_primos(numero, final):
+    if numero > final:
+        return []
+    
+
+    if num_primo(numero, 2):
+        return [numero] + listar_primos(numero + 1, final)
+        
+    return listar_primos(numero + 1, final)
+    
+    
+def encontrar_diferencas(lista):
+    if len(lista) < 2:
+        return []
+        
+    m = cauda(lista)
+    c = cabeca(lista)
+    c2 = cabeca(m)
+    
+    return [c2 -c] + encontrar_diferencas(m)
+    
+
+def encontrar_maior(lista, comb):
+    if len(lista) == 0:
+        return 0
+    
+    m = encontrar_maior(cauda(lista), comb)
+    c = cabeca(lista)
+    
+    return comb(c,m)
+    
+    
+def main():
+    inicio = int(input().rstrip())
+    final = int(input().rstrip())
+
+    lista_primos = listar_primos(inicio, final)
+    lista_diferencas = encontrar_diferencas(lista_primos)
+    maior_diferenca = encontrar_maior(lista_diferencas, lambda x,y: x  if x > y else y)
+
+
+    print(maior_diferenca)
+
+    
+if __name__ == "__main__":
+    main()
